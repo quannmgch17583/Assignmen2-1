322a480ed27dba0b97058bec69d64401ae9c2a8e
quannmgch17583 <quannmgch17583@fpt.edu.vn>
2019-11-01 21:57:22 +0700

    haha

---
+++ Product.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method = "post" action = "doProduct.php">
+		<table>
+			<tr>
+				<td>Name</td>
+				<td><input type = "text" name= "txtName"></td>
+			
+			<tr>
+				<td>Price</td>
+				<td><input type = "text" name = "txtprice"/></td>
+			</tr>
+			
+			<tr>
+				<td></td>
+				<td><input type = "submit" name = "Submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
